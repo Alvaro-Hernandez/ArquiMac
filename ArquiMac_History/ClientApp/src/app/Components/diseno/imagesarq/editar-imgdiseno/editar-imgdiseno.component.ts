25ac07e7b86a52bdad91d28fa85ff754ba98badb
Alvaro-Hernandez <alvaropineda606@gmail.com>
2022-11-12 02:00:34 -0600

    Creando Servicio de PutImgDiseno

--- ArquiMac_History/ClientApp/src/app/Components/diseno/imagesarq/editar-imgdiseno/editar-imgdiseno.component.ts
+++ ArquiMac_History/ClientApp/src/app/Components/diseno/imagesarq/editar-imgdiseno/editar-imgdiseno.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FuncionImagenArqService } from 'src/app/Services/funcion-imagen-arq.service';
+import { ImagenDiseno } from '../../../../Models/ImagenDiseno';
 
 @Component({
   selector: 'app-editar-imgdiseno',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarImgdisenoComponent implements OnInit {
 
-  constructor() { }
+    constructor(private service: FuncionImagenArqService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+    dataImgArq: ImagenDiseno;
 
+    editarFormImgArq = new FormGroup({
+        id_Imagen: new FormControl(''),
+        id_Diseno: new FormControl(''),
+        almacenamiento: new FormControl(''),
+        error: new FormControl('')
+    });
+
+    ngOnInit() {
+        let imgDisenoId = this.activatedRoute.snapshot.paramMap.get('id_Imagen');
+        this.service.getImgDisenoId(imgDisenoId).subscribe(Data => {
+            this.dataImgArq = Data;
+            this.editarFormImgArq.patchValue({
+                'id_Imagen': this.dataImgArq.id_Imagen,
+                'id_Diseno': this.dataImgArq.id_Diseno,
+                'almacenamiento': this.dataImgArq.almacenamiento,
+            });
+        });
+    }
+
+    putImgDiseno(form: ImagenDiseno) {
+        this.service.putImgDiseno(form).subscribe(Data => {
+            console.log(Data);
+            alert("Datos Editados");
+        });
+        window.localStorage.reload;
+        this.router.navigate(["ListarImagesarq"]);
+    }
+
+    Salir() {
+        this.router.navigate(["ListarImagesarq"]);
+    }
 }
